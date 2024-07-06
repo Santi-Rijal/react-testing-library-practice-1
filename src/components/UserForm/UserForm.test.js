f04651824e37ce3c10b33b5eb6b28c1db37eb439
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-07-06 13:50:25 -0300

    Added 2 new tests

--- src/components/UserForm/UserForm.test.js
+++ src/components/UserForm/UserForm.test.js
@@ -1,6 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import UserForm from "./UserForm";
+import { act } from "react";
+
+const fillFormAndSubmit = (callback) => {
+  render(<UserForm onUserAdd={callback} />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const addBtn = screen.getByRole("button");
+
+  userEvent.click(nameInput);
+  userEvent.keyboard("Santi");
+
+  userEvent.click(emailInput);
+  userEvent.keyboard("123@dal.ca");
+
+  act(() => {
+    userEvent.click(addBtn);
+  });
+
+  return { nameInput, emailInput };
+};
 
 test("there are 2 inputs and 1 button", () => {
   render(<UserForm />);
@@ -15,24 +36,18 @@ test("there are 2 inputs and 1 button", () => {
 test("it calls onUserAdd callback fn when form is submitted", () => {
   const onUserAddMock = jest.fn();
 
-  render(<UserForm onUserAdd={onUserAddMock} />);
-
-  const nameInput = screen.getByRole("textbox", { name: /name/i });
-  const emailInput = screen.getByRole("textbox", { name: /email/i });
-
-  userEvent.click(nameInput);
-  userEvent.keyboard("Santi");
-
-  userEvent.click(emailInput);
-  userEvent.keyboard("123@dal.ca");
-
-  const addBtn = screen.getByRole("button");
-
-  userEvent.click(addBtn);
+  fillFormAndSubmit(onUserAddMock);
 
   expect(onUserAddMock).toHaveBeenCalled();
   expect(onUserAddMock).toHaveBeenCalledWith({
     name: "Santi",
     email: "123@dal.ca",
   });
+});
+
+test("input are empty after form submit", () => {
+  const { nameInput, emailInput } = fillFormAndSubmit(() => {});
+
+  expect(nameInput).toHaveValue("");
+  expect(emailInput).toHaveValue("");
 });
